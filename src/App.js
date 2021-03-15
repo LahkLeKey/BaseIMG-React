cf82cb3ae2bfbb1780e2d8515e09b3f0511ef109
LahkLeKey <53668012+LahkLeKey@users.noreply.github.com>
2021-03-14 19:11:57 -0500

    Basic react app docker image.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br></br>Docker image starter for LahkLeKey
         </p>
         <a
           className="App-link"
